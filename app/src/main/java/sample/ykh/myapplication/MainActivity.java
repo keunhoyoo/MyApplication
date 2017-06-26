b5e3009ca28650c4cfe51a6df60b8f370f25f09a
keunho.yoo <keunho.yoo@gmail.com>
2017-06-26 18:05:36 +0900

    1

--- app/src/main/java/sample/ykh/myapplication/MainActivity.java
+++ app/src/main/java/sample/ykh/myapplication/MainActivity.java
@@ -2,16 +2,198 @@ package sample.ykh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import org.webrtc.Camera2Enumerator;
+import org.webrtc.CameraEnumerator;
+import org.webrtc.DataChannel;
+import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
+import org.webrtc.Logging;
+import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
+import org.webrtc.RendererCommon;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "MainActivity";
+
+    private class PCObserver implements PeerConnection.Observer {
+
+        @Override
+        public void onSignalingChange(PeerConnection.SignalingState signalingState) {
+            Log.d(TAG, "onSignalingChange");
+        }
+
+        @Override
+        public void onIceConnectionChange(PeerConnection.IceConnectionState iceConnectionState) {
+            Log.d(TAG, "onIceConnectionChange");
+        }
+
+        @Override
+        public void onIceConnectionReceivingChange(boolean b) {
+            Log.d(TAG, "onIceConnectionReceivingChange");
+        }
+
+        @Override
+        public void onIceGatheringChange(PeerConnection.IceGatheringState iceGatheringState) {
+            Log.d(TAG, "onIceGatheringChange");
+        }
+
+        @Override
+        public void onIceCandidate(IceCandidate iceCandidate) {
+            Log.d(TAG, "onIceCandidate");
+        }
+
+        @Override
+        public void onIceCandidatesRemoved(IceCandidate[] iceCandidates) {
+            Log.d(TAG, "onIceCandidatesRemoved");
+        }
+
+        @Override
+        public void onAddStream(MediaStream mediaStream) {
+            Log.d(TAG, "onAddStream");
+        }
+
+        @Override
+        public void onRemoveStream(MediaStream mediaStream) {
+            Log.d(TAG, "onRemoveStream");
+        }
+
+        @Override
+        public void onDataChannel(DataChannel dataChannel) {
+            Log.d(TAG, "onDataChannel");
+        }
+
+        @Override
+        public void onRenegotiationNeeded() {
+            Log.d(TAG, "onRenegotiationNeeded");
+        }
+    }
+
+    PeerConnection peerConnection;
+    PCObserver pcObserver = new PCObserver();
+    SurfaceViewRenderer localView = null;
+    VideoCapturer videoCapturer;
+    EglBase eglBase;
+    Executor executor = Executors.newSingleThreadScheduledExecutor();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
         setContentView(R.layout.activity_main);
 
+        PeerConnectionFactory.initializeAndroidGlobals(this, true, true, true);
+
         PeerConnectionFactory factory = new PeerConnectionFactory(new PeerConnectionFactory.Options());
+
+        List<PeerConnection.IceServer> iceServers = new ArrayList<PeerConnection.IceServer>();
+        iceServers.add(new PeerConnection.IceServer("stun:stun.l.google.com:19302"));
+
+        peerConnection = factory.createPeerConnection(iceServers, new MediaConstraints(), pcObserver);
+        Log.d(TAG, "factory.createPeerConnection");
+
+
+
+
+        Camera2Enumerator camEnum = new Camera2Enumerator(this);
+        videoCapturer = createCameraCapturer(camEnum);
+
+
+        VideoSource videoSource = factory.createVideoSource(videoCapturer);
+
+
+        VideoTrack videoTrack = factory.createVideoTrack("ARDAMSv0", videoSource);
+        videoTrack.setEnabled(true);
+
+
+
+        localView = (SurfaceViewRenderer)findViewById(R.id.local_video_view);
+
+        eglBase = EglBase.create();
+        factory.setVideoHwAccelerationOptions(eglBase.getEglBaseContext(), eglBase.getEglBaseContext());
+        localView.init(eglBase.getEglBaseContext(), null);
+        localView.setEnableHardwareScaler(true);
+        localView.setZOrderMediaOverlay(true);
+        localView.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
+//        localView.setMirror(true);
+//        localView.requestLayout();
+
+        videoTrack.addRenderer(new VideoRenderer(localView));
+
+        MediaStream localMediaStream = factory.createLocalMediaStream("ARDAMS");
+
+        localMediaStream.addTrack(videoTrack);
+        peerConnection.addStream(localMediaStream);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                    Log.d(TAG, "Restart video source.");
+                videoCapturer.startCapture(176, 144, 20);
+                }
+        });
+
+
+
+
     }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        // First, try to find front facing camera
+        Logging.d(TAG, "Looking for front facing cameras.");
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating front facing camera capturer. " + deviceName);
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        // Front facing camera not found, try something else
+        Logging.d(TAG, "Looking for other cameras.");
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating other camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
+    }
+
 }
